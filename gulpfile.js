8f13fa4048f3459ade31dc5e6e702a0df17f07ef
miya0001 <miya@wpist.me>
2015-11-04 06:49:40 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gulp = require('gulp');
+var csv2json = require('gulp-csv2json');
+var rename = require('gulp-rename');
+
+gulp.task('default', function () {
+    var csvParseOptions = {}; //based on options specified here : http://csv.adaltas.com/parse/
+    gulp.src('sample.csv')
+        .pipe(csv2json(csvParseOptions))
+        .pipe(rename({extname: '.json'}))
+        .pipe(gulp.dest('.'));
+});
