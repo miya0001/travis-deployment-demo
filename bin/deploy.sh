e7c57fdab7022c557fd9dbed41344c66370e476c
miya0001 <miya@wpist.me>
2015-11-04 07:30:12 +0900

    update env vars

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -29,4 +29,4 @@ git config user.name "Travis CI"
 git config user.email "miya+github.com@wpist.me"
 git add .
 git commit --quiet -m "Deploy from travis"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
